d1c0e100cd5a9fd8539073880332e859210e5e3e
rodolfo1979 <rfallas2011@gmail.com>
2026-09-03 10:28:45 -0600

    Refine seller desktop sales layout

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vendedor-v21';
+const CACHE_NAME = 'vendedor-v22';
 const ARCHIVOS = ['index.html', 'app.js', 'ui.js', 'manifest.json'];
 
 self.addEventListener('install', (event) => {
